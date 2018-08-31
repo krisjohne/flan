7fb6713f9de8bf797f133a23db7c1709efbb0c66
krisjohne <estrabillo.kristianjohn@gmail.com>
2018-08-30 18:42:13 -0700

    Added form element and button div to index.html. Added code to process form data in script.js.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("script.js running...");
+console.log("script.js is running...");
+
+let numberTag = document.querySelector("#number");
+const button = document.querySelector("#button");
+
+let num = Number(numberTag.innerHTML)
+console.log(num);
+
+function myFunction() {
+  let x = document.forms["myForm"]["add"].value;
+  x = Number(x);
+  console.log(x);
+
+  num = x + num;
+  numberTag.innerHTML = num;
+}
+
+button.addEventListener('click', e => {
+  console.log("button clicked");
+  myFunction();
+});
