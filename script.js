f69ce6ed8f9f1f905f5c374a9ad6d1529cd24813
krisjohne <estrabillo.kristianjohn@gmail.com>
2018-08-30 19:00:27 -0700

    Added more tests, changed variable names to be more meaningful.

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 console.log("script.js is running...");
 
 let numberTag = document.querySelector("#number");
+let dropResult = document.querySelector("#drop-result");
+
 const button = document.querySelector("#button");
 
-let num = Number(numberTag.innerHTML)
-console.log(num);
+let initial_num = Number(numberTag.innerHTML)
+console.log(initial_num);
 
-function myFunction() {
-  let x = document.forms["myForm"]["add"].value;
-  x = Number(x);
-  console.log(x);
+function add_to_numberTag() {
+  let addend = document.forms["myForm"]["add"].value;
+  addend = Number(addend);
+  console.log(addend);
 
-  num = x + num;
-  numberTag.innerHTML = num;
+  let sum = addend + initial_num;
+  numberTag.innerHTML = sum;
+}
+
+function show_dropdown_result() {
+  let selected_value = document.forms["myForm"]["dropdown"].value;
+
+  if (selected_value == "1") {
+    dropResult.innerHTML = "1 was selected";
+  } else if (selected_value == "2") {
+    dropResult.innerHTML = "2 was selected";
+  }
+
 }
 
 button.addEventListener('click', e => {
   console.log("button clicked");
-  myFunction();
+  add_to_numberTag();
+  show_dropdown_result();
 });
